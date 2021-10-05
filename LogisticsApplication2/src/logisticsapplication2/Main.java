bd84599010cc1f4c6ba448823009b732e87d472c
zjhozella <91499400+zjhozella@users.noreply.github.com>
2021-10-05 14:33:06 -0400

    Update Main

--- LogisticsApplication2/src/logisticsapplication2/Main.java
+++ LogisticsApplication2/src/logisticsapplication2/Main.java
@@ -15,9 +15,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
-        System.out.println("Test");
+        // Take input from ConfirmationView, feed into Load object constructor
+        public void CreateLoad(){
+            
+        }
+        
+        // Send Load object attributes to database using an sql statement
+        
+        
+        
     }
     
 }
