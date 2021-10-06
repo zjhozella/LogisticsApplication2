e38c9fab0dbae0fd87e861ca4631a0c07c3c7220
Joey Prince <50034358+strifef7@users.noreply.github.com>
2021-10-05 20:47:56 -0400

    getters and setters

--- LogisticsApplication2/src/model/Load.java
+++ LogisticsApplication2/src/model/Load.java
@@ -19,68 +19,121 @@ public class Load {
     private int sealNumber    = 0;
     private int driverNumber  = 0;
     private boolean loadComplete = false;
-    Timestamp outgoingTime = new Timestamp(2000);
+    private Timestamp outgoingTime = new Timestamp(2000);
     //Need to input timestamps
 
 
     // Constructor for load class
     public Load (int newTrailerNumber, int newTruckNumber, int newStoreNumber , int newSealNumber , int newDriverNumber ,
                  boolean newLoadComplete, Timestamp outgoingTime ){
-        this.setTrailerNumber(newTrailerNumber);
-        this.setTruckNumber(newTruckNumber);
-        this.setStoreNumber(newStoreNumber);
-        this.setSealNumber(newSealNumber);
-        this.setDriverNumber(newDriverNumber);
-        this.setLoadComplete(newLoadComplete);
+        this.trailerNumber = newTrailerNumber;
+        this.truckNumber   = newTruckNumber;
+        this.storeNumber   = newStoreNumber;
+        this.sealNumber    = newSealNumber;
+        this.driverNumber  = newDriverNumber;
+        this.loadComplete  = newLoadComplete;
+        
         // Need to figure this part out still
         System.out.println(outgoingTime.toString());
     }
 
+    /**
+     * @return the truckNumber
+     */
     public int getTruckNumber() {
         return truckNumber;
     }
 
+    /**
+     * @param truckNumber the truckNumber to set
+     */
     public void setTruckNumber(int truckNumber) {
         this.truckNumber = truckNumber;
     }
 
+    /**
+     * @return the trailerNumber
+     */
     public int getTrailerNumber() {
         return trailerNumber;
     }
 
+    /**
+     * @param trailerNumber the trailerNumber to set
+     */
     public void setTrailerNumber(int trailerNumber) {
         this.trailerNumber = trailerNumber;
     }
 
+    /**
+     * @return the storeNumber
+     */
     public int getStoreNumber() {
         return storeNumber;
     }
 
+    /**
+     * @param storeNumber the storeNumber to set
+     */
     public void setStoreNumber(int storeNumber) {
         this.storeNumber = storeNumber;
     }
 
+    /**
+     * @return the sealNumber
+     */
     public int getSealNumber() {
         return sealNumber;
     }
 
+    /**
+     * @param sealNumber the sealNumber to set
+     */
     public void setSealNumber(int sealNumber) {
         this.sealNumber = sealNumber;
     }
 
+    /**
+     * @return the driverNumber
+     */
     public int getDriverNumber() {
         return driverNumber;
     }
 
+    /**
+     * @param driverNumber the driverNumber to set
+     */
     public void setDriverNumber(int driverNumber) {
         this.driverNumber = driverNumber;
     }
 
+    /**
+     * @return the loadComplete
+     */
     public boolean isLoadComplete() {
         return loadComplete;
     }
 
+    /**
+     * @param loadComplete the loadComplete to set
+     */
     public void setLoadComplete(boolean loadComplete) {
         this.loadComplete = loadComplete;
     }
+
+    /**
+     * @return the outgoingTime
+     */
+    public Timestamp getOutgoingTime() {
+        return outgoingTime;
+    }
+
+    /**
+     * @param outgoingTime the outgoingTime to set
+     */
+    public void setOutgoingTime(Timestamp outgoingTime) {
+        this.outgoingTime = outgoingTime;
+    }
+
+  
 }
--- LogisticsApplication2/src/model/driver.java
+++ LogisticsApplication2/src/model/driver.java
@@ -13,10 +13,10 @@ public class driver {
     // What else should be put in here.
     // 
     //Default constructor
-    int dlNumber     = 0; 
-    String firstName = "";
-    String lastName  = "";
-    String company   = ""; //Should we track company as a string or assign as a company number?
+    private int dlNumber     = 0; 
+    private String firstName = "";
+    private String lastName  = "";
+    private String company   = ""; //Should we track company as a string or assign as a company number?
 
 
     //Constructor for driver class
@@ -25,6 +25,63 @@ public class driver {
         this.firstName = newFirstName;
         this.lastName  = newLastName;
         this.company   = newCompany;
+    }
+
+    //Getters and setters for driver class
+    /**
+     * @return the dlNumber
+     */
+    public int getDlNumber() {
+        return dlNumber;
+    }
+
+    /**
+     * @param dlNumber the dlNumber to set
+     */
+    public void setDlNumber(int dlNumber) {
+        this.dlNumber = dlNumber;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the company
+     */
+    public String getCompany() {
+        return company;
+    }
+
+    /**
+     * @param company the company to set
+     */
+    public void setCompany(String company) {
+        this.company = company;
     }
     
 }
