747c32c683abb2af0fa1ad525b4d3ca3bb4037fa
Joey Prince <50034358+strifef7@users.noreply.github.com>
2021-10-06 13:55:54 -0400

    Update Load.java

--- LogisticsApplication2/src/model/Load.java
+++ LogisticsApplication2/src/model/Load.java
@@ -20,6 +20,7 @@ public class Load {
     String[] dunnage = {"empty", "1/4", "1/2", "3/4", "Full"};
 
     private int storeNumber   = 0;
+    int loadNumber = 0;
     private int sealNumber    = 0;
     private int driverNumber  = 0;
     private boolean loadComplete = false;
@@ -30,8 +31,9 @@ public class Load {
     Timestamp tsIn = new Timestamp(time);
 
     // Constructor for load class
-    public Load (int loadNumber, int newTruckNumber, int newTrailerNumber, int newStoreNumber , int newSealNumber , int newDriverNumber ,
+    public Load (int newloadNumber, int newTruckNumber, int newTrailerNumber, int newStoreNumber , int newSealNumber , int newDriverNumber ,
                  boolean newLoadComplete, Timestamp newTSout, Timestamp newTSin){
+        this.loadNumber    = newloadNumber;
         this.trailerNumber = newTrailerNumber;
         this.truckNumber   = newTruckNumber;
         this.storeNumber   = newStoreNumber;
@@ -43,90 +45,6 @@ public class Load {
 
     }
 
-    // Getter and setters for load
-    /**
-     * @return the truckNumber
-     */
-    public int getTruckNumber() {
-        return truckNumber;
-    }
-
-    /**
-     * @param truckNumber the truckNumber to set
-     */
-    public void setTruckNumber(int truckNumber) {
-        this.truckNumber = truckNumber;
-    }
-
-    /**
-     * @return the trailerNumber
-     */
-    public int getTrailerNumber() {
-        return trailerNumber;
-    }
-
-    /**
-     * @param trailerNumber the trailerNumber to set
-     */
-    public void setTrailerNumber(int trailerNumber) {
-        this.trailerNumber = trailerNumber;
-    }
-
-    /**
-     * @return the storeNumber
-     */
-    public int getStoreNumber() {
-        return storeNumber;
-    }
-
-    /**
-     * @param storeNumber the storeNumber to set
-     */
-    public void setStoreNumber(int storeNumber) {
-        this.storeNumber = storeNumber;
-    }
-
-    /**
-     * @return the sealNumber
-     */
-    public int getSealNumber() {
-        return sealNumber;
-    }
-
-    /**
-     * @param sealNumber the sealNumber to set
-     */
-    public void setSealNumber(int sealNumber) {
-        this.sealNumber = sealNumber;
-    }
-
-    /**
-     * @return the driverNumber
-     */
-    public String getDriverNumber() {
-        return driverNumber;
-    }
-
-    /**
-     * @param driverNumber the driverNumber to set
-     */
-    public void setDriverNumber(String driverNumber) {
-        this.driverNumber = driverNumber;
-    }
-
-    /**
-     * @return the loadComplete
-     */
-    public boolean isLoadComplete() {
-        return loadComplete;
-    }
-
-    /**
-     * @param loadComplete the loadComplete to set
-     */
-    public void setLoadComplete(boolean loadComplete) {
-        this.loadComplete = loadComplete;
-    }
 
   
 }
