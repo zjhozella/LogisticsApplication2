c2fd50d14fccd57c86b129162dfccfd245a4afb0
Joey Prince <50034358+strifef7@users.noreply.github.com>
2021-10-11 00:30:01 -0400

    Update outgoingTrans.java

--- LogisticsApplication2/src/model/outgoingTrans.java
+++ LogisticsApplication2/src/model/outgoingTrans.java
@@ -10,12 +10,40 @@ package model;
  * @author Jprince
  */
 public class outgoingTrans extends Transaction {
-    int employeeID = 0;
-    boolean insectDetected = false;
+    private int employeeID = 0;
+    private boolean insectDetected = false;
 
     outgoingTrans(int transID, Load loadNumber, int employeeID, boolean insectDetected){
         super(transID, loadNumber);
         this.employeeID = employeeID;
+        this.insectDetected = insectDetected;
+    }
+
+    /**
+     * @return the employeeID
+     */
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    /**
+     * @param employeeID the employeeID to set
+     */
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    /**
+     * @return the insectDetected
+     */
+    public boolean isInsectDetected() {
+        return insectDetected;
+    }
+
+    /**
+     * @param insectDetected the insectDetected to set
+     */
+    public void setInsectDetected(boolean insectDetected) {
         this.insectDetected = insectDetected;
     }
 }
