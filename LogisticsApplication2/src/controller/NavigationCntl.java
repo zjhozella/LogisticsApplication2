0093dea59fd4f1dcebc53cd0291e0e8146a3c6c2
Joey Prince <50034358+strifef7@users.noreply.github.com>
2021-11-09 11:30:07 -0500

    Update NavigationCntl.java

--- LogisticsApplication2/src/controller/NavigationCntl.java
+++ LogisticsApplication2/src/controller/NavigationCntl.java
@@ -13,30 +13,43 @@ import java.util.Scanner;
  */
 public class NavigationCntl { //NavigationCntl should call NavView to print
     Scanner scnr = new Scanner(System.in);
+    boolean nav = true;
 
     public NavigationCntl(){
-        NavigationView navMenu= new NavigationView();
-        int option = scnr.nextInt();
+
+        //Wrapped in a while loop, to handle unexpected input.
+        //Can change behavior by editing status of nav in each case.
+        //Currently program sends to selected controller then EXITS. 
+        //If nav changed to true in case then menu would re-load.
+        while(nav){
+            NavigationView navMenu= new NavigationView();
+            int option = scnr.nextInt();
         
-        switch (option){
-            case 1: 
-                OutgoingCntl outLoad = new OutgoingCntl();
-                break;
-            case 2:
-                IncomingCntl inLoad = new IncomingCntl();
-                break;
-            case 3:
-                editOutLoadCntl editOutLoad = new editOutLoadCntl();
-                break;
-            case 4: 
-                editInLoadCntl editInLoad = new editInLoadCntl();
-                break;
-            case 9:
-                System.out.println("Goodbye!");
-                System.exit(0);
-            default: 
-                System.out.println("Invalid Option, choose again!");
-                break;
+
+            switch (option){
+                case 1: 
+                    OutgoingCntl outLoad = new OutgoingCntl();
+                    nav = false;
+                    break;
+                case 2:
+                    IncomingCntl inLoad = new IncomingCntl();
+                    nav = false;
+                    break;
+                case 3:
+                    editOutLoadCntl editOutLoad = new editOutLoadCntl();
+                    nav = false;
+                    break;
+                case 4: 
+                    editInLoadCntl editInLoad = new editInLoadCntl();
+                    nav = false;
+                    break;
+                case 9:
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                default: 
+                    System.out.println("Invalid Option, choose again!");
+                    break;
+            }
         }
 
     }
