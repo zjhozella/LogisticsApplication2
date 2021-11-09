be20fa01f7e2b7bf7997dc9e0d4e669a43359b30
Joey Prince <50034358+strifef7@users.noreply.github.com>
2021-11-09 14:22:12 -0500

    r
    r

--- LogisticsApplication2/src/controller/editOutLoadCntl.java
+++ LogisticsApplication2/src/controller/editOutLoadCntl.java
@@ -1,9 +1,19 @@
 package controller;
 import model.*;
+import view.*;
+import java.util.Scanner;
+import java.util.Set;
 
 public class editOutLoadCntl{
+    Scanner scnr = new Scanner(System.in);
+    boolean nav = true;
 
     public  editOutLoadCntl(){
-        System.out.println("Welcome to editLoadCntl");
+        //Test for navigation
+        System.out.println("Welcome to editOutLoadCntl");
+        editOutView editOut = new editOutView();
+        
     }
+
+
 }
---
+++ LogisticsApplication2/src/view/editOutView.java
@@ -0,0 +1,33 @@
+package view;
+
+//import controller.editOutLoadCntl;
+import model.Model;
+import java.util.Scanner;
+
+public class editOutView {
+    Scanner scnr = new Scanner(System.in);
+    public editOutView(){
+        System.out.println("Welcome to the edit menu for [Outgoing Load]");
+        System.out.print("Please enter load number to edit: ");
+        int loadNum = scnr.nextInt();
+        System.out.println("Current Load information: ");
+        System.out.println(Model.getOutList().get(loadNum).printLoad());
+
+        // Menu For editing outTrans paramaters
+        // Not all parameters are editable for security reasons
+        System.out.println("Select an attribute to edit from list below: ");
+        System.out.println("(1) - Edit Truck Number");
+        System.out.println("(2) - Edit Trailer Number");
+        System.out.println("(3) - Edit Store Number");
+        System.out.println("(4) - Edit Seal Number");
+        System.out.println("(5) - Edit Driver Number");
+        System.out.println("(6) - Edit Insect Status");
+        System.out.println("(9) - Exit");
+
+
+    }
+
+
+    
+
+}
