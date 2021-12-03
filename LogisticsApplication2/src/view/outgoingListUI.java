5dbc199a53bf7ee421d9098a9600726355a3107e
Joey Prince <50034358+strifef7@users.noreply.github.com>
2021-12-02 21:46:26 -0500

    Made Comments for Mr. G

--- LogisticsApplication2/src/view/outgoingListUI.java
+++ LogisticsApplication2/src/view/outgoingListUI.java
@@ -23,7 +23,11 @@ public class outgoingListUI extends JFrame{
         JPanel buttonPanel = new JPanel(new GridLayout(1,4));
 
         //OutgoingCntl outCntl = new OutgoingCntl();
-        //outTable = new JTable(Model.getOutList().get());//HELP!!!!
+        //outTable = new JTable(Model.getOutList().get());//HELP!!!! 
+        
+        /*This is where I am having the most trouble. I can not get the Outgoing load arraylist into the outTable JTable. I can follow in Mr. G's example where his data is comming in from but ours is flowing from either the OutgoingTableModel (from the outList) or can it flow from the outLoadList (outgoingLoads arraylist) or should it come from our Model.getOutList??? Once we figure where to load the table from I think the rest will come along easier. There must be something silly
+        that I am missing (show me the way young skywalker!!). JWP 12/02
+        */
         outTable = new JTable();
 
     }
