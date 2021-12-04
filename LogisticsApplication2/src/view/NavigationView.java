8b6a442a18b7992be942538f61245ce1a0c104a5
zjhozella <91499400+zjhozella@users.noreply.github.com>
2021-12-04 02:35:09 -0500

    Non functional NavigationView UI

--- LogisticsApplication2/src/view/NavigationView.java
+++ LogisticsApplication2/src/view/NavigationView.java
@@ -6,9 +6,13 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -17,7 +21,7 @@ import javax.swing.JPanel;
  */
 public class NavigationView extends JFrame{
     
-    public JPanel buttonPanel;
+    public JPanel titlePanel, centerPanel, buttonPanel;
     public JButton newOut, newIn, listOut, listIn, exit;
     
     //Will contain a frame, a panel within that frame, and 2 buttons (Outgoing, Incoming).
@@ -29,25 +33,39 @@ public class NavigationView extends JFrame{
     }
     
     public void initNav(){
+        centerPanel = new JPanel(new GridLayout(1, 1));
         buttonPanel = new JPanel(new GridLayout(5,1));
-        this.add(buttonPanel);
-        newOut = new JButton("Create Outgoing");
-        newIn = new JButton("Create Incoming");
-        listOut = new JButton("List Outgoing");
-        listIn = new JButton("List Incoming");
-        exit = new JButton("Exit");
+        newOut = new JButton("New Outgoing Load");
+        newIn = new JButton("New Incoming Load");
+        listOut = new JButton("View All Outgoing Loads");
+        listIn = new JButton("View All Incoming Loads");
+        exit = new JButton("Exit Application");
+        
+        centerPanel.setBorder(BorderFactory.createEmptyBorder(0, 50, 0, 50));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25));
+        
         buttonPanel.add(newOut);
         buttonPanel.add(newIn);
         buttonPanel.add(listOut);
         buttonPanel.add(listIn);
         buttonPanel.add(exit);
         
-        this.setSize(400, 500);
+        centerPanel.add(buttonPanel);
+        titlePanel = new JPanel(new FlowLayout());
+        titlePanel.add(new JLabel("<html><h2 style=\"text-align:center;\">Welcome to Logistics!</h2><p style=\"text-align:center;\">Please select from the following menu options:</p></html>"));
+  
+        
+        this.setSize(700, 600);
         this.setLocationRelativeTo(null);
         this.setContentPane(new JPanel(new BorderLayout()));
+        this.getContentPane().add(titlePanel, BorderLayout.NORTH);
         this.getContentPane().add(buttonPanel, BorderLayout.CENTER);
+        this.setTitle("Logistics Application");
+        this.pack();
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
     }
     
     //Prints menu for the user to select a choice
